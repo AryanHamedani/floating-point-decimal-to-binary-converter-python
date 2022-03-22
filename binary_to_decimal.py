e9c349257b53092a9dd4d9c3bf284134d6fc5a24
Aryan <aryn.hmd@gmail.com>
2022-03-22 11:53:23 +0430

    Very First Version

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,9 +1,9 @@
-def binary_converter (dec_num):
-    #spliting integer part and floating point part by '.' and store them in dec_whole and dec_dec
-    dec_whole,dec_dec = map(str,dec_num.split('.'))
-    #converting integer part to binary form using .format and store that in bin_whole
+def binary_converter(dec_num):
+    # splitting integer part and floating point part by '.' and store them in dec_whole and dec_dec
+    dec_whole, dec_dec = map(str, dec_num.split('.'))
+    # converting integer part to binary form using .format and store that in bin_whole
     bin_whole = '{:b}'.format(int(dec_whole))
-    #store '' in binary decimal part and looping over decimal floating point untill its gone or untill we want
+    # store '' in binary decimal part and looping over decimal floating point until its gone or until we want
     bin_dec = ''
     while int(dec_dec) != 0 and len(bin_dec) < 5:
         if len(str(int(dec_dec)*2)) > len(dec_dec):
@@ -12,5 +12,5 @@ def binary_converter (dec_num):
         else:
             bin_dec += '0'
             dec_dec = str(int(dec_dec) * 2)
-    #returning binary floating point number string
-    return('{bin_whole}.{bin_dec}'.format(bin_whole = bin_whole , bin_dec = bin_dec))
+    # returning binary floating point number string
+    return'{bin_whole}.{bin_dec}'.format(bin_whole=bin_whole, bin_dec=bin_dec)
